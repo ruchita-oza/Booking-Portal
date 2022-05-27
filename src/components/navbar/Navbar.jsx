a6644e31bcee75c7949ed65d5878f47ac49da3dd
Hetvee Sakaria <hetvee@atliq.com>
2022-05-27 12:45:40 +0530

    Frontend - navbar Done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,10 +5,12 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        <span className="logo">Skyline booking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
+          <button className="navButton">Logout</button>   
+          <button className="navButton">User Profile</button>   
         </div>
       </div>
     </div>
