a52d3622e0e6d9228f02661a1e3a6745d92ff00c
Hetvee Sakaria <hetvee@atliq.com>
2022-06-03 10:22:01 +0530

    Card

--- client/src/components/transport/Transport.jsx
+++ client/src/components/transport/Transport.jsx
@@ -33,27 +33,49 @@ const transport = () => {
          </div>
         </div>
         <div class="card w-100 carousel-item">
-        <div className='card d-block mb-5 shadow rounded'>
-           <div className='row d-flex justify-content-between'>
-             <div className='col-lg-3  col-md-3 d-flex justify-content-center ml-3'>
-               <img
-                 src='https://content.presentermedia.com/content/animsp/00007000/7148/airplane_passenger_ride_md_nwm_v2.gif'
-                 alt='true'
-               />
-             </div>
-             <div className='col-lg-8 col-md-8 d-flex align-items-center justify-content-center'>
-               <div>
-                 <h5>Domestic/International Flights</h5>
-                 <p>
-                   Call +91-976354788 for any help and qery. <br /> Call
-                   +91-8320028900 for booking.
-                 </p>
-               </div>
-             </div>
-           </div>
-         </div>
+          <div className='card d-block mb-5 shadow rounded'>
+            <div className='row d-flex justify-content-between'>
+              <div className='col-md-3 d-flex justify-content-center ml-3'>
+                <img
+                  src='https://content.presentermedia.com/content/animsp/00007000/7148/airplane_passenger_ride_md_nwm_v2.gif'
+                  alt='true'
+                />
+              </div>
+              <div className='col-lg-8 col-md-8 d-flex align-items-center justify-content-center'>
+                <div>
+                  <h5>Book You Train</h5>
+                  <p>
+                    Call +91-976354788 for any help and qery. <br /> Call
+                    +91-8320028900 for booking.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card w-100 carousel-item">
+          <div className='card d-block mb-5 shadow rounded'>
+            <div className='row d-flex justify-content-between'>
+              <div className='col-lg-3  col-md-3 d-flex justify-content-center ml-3'>
+                <img
+                  src='https://content.presentermedia.com/content/animsp/00007000/7148/airplane_passenger_ride_md_nwm_v2.gif'
+                  alt='true'
+                />
+              </div>
+              <div className='col-lg-8 col-md-8 d-flex align-items-center justify-content-center'>
+                <div>
+                  <h5>Enjoy your ride in Supex Flexible Bus</h5>
+                  <p>
+                    Call +91-976354788 for any help and qery. <br /> Call
+                    +91-8320028900 for booking.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
+    
     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
