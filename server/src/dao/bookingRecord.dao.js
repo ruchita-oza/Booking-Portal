334d15217494c15e19c93394f83e48e26b63b803
RikinChauhan <rikin@atliq.com>
2022-06-09 18:03:50 +0530

    completed and upcoming booking details created and tested

---
+++ server/src/dao/bookingRecord.dao.js
@@ -0,0 +1,141 @@
+const db = require("../models");
+const TrainSchedule = db.train_schedules;
+const BusSchedule = db.bus_schedule;
+const FlightSchedule = db.flight_schedule;
+const BookingRecord = db.booking_records;
+const { findTrainScheduleById } = require("../dao/train.dao");
+const { findFlightScheduleById } = require("../dao/flight.dao");
+const { findBusScheduleById } = require("../dao/bus.dao");
+const City = db.cities;
+
+TrainSchedule.hasMany(BookingRecord, {
+  // as: "train_schedule",
+  foreignKey: "transport_id",
+});
+BookingRecord.belongsTo(TrainSchedule, { foreignKey: "transport_id" });
+
+BusSchedule.hasMany(BookingRecord, {
+  // as: "bus_schedule",
+  foreignKey: "transport_id",
+});
+BookingRecord.belongsTo(BusSchedule, { foreignKey: "transport_id" });
+
+FlightSchedule.hasMany(BookingRecord, {
+  //   as: "flight_schedule",
+  foreignKey: "transport_id",
+});
+BookingRecord.belongsTo(FlightSchedule, { foreignKey: "transport_id" });
+
+const findBookingRecordsByUserId = async (userId) => {
+  let flightBookingRecords = await BookingRecord.findAll({
+    where: { cust_id: userId, transport_type: "flight" },
+    include: [
+      {
+        model: FlightSchedule,
+        include: [
+          {
+            model: City,
+            as: "source_name",
+            attributes: ["city_name"],
+          },
+          {
+            model: City,
+            as: "destination_name",
+            attributes: ["city_name"],
+          },
+        ],
+      },
+    ],
+  });
+
+  let busBookingRecords = await BookingRecord.findAll({
+    where: { cust_id: userId, transport_type: "bus" },
+    include: [
+      {
+        model: BusSchedule,
+        include: [
+          {
+            model: City,
+            as: "source_name",
+            attributes: ["city_name"],
+          },
+          {
+            model: City,
+            as: "destination_name",
+            attributes: ["city_name"],
+          },
+        ],
+      },
+    ],
+  });
+
+  let trainBookingRecords = await BookingRecord.findAll({
+    where: { cust_id: userId, transport_type: "train" },
+    include: [
+      {
+        model: TrainSchedule,
+        include: [
+          {
+            model: City,
+            as: "source_name",
+            attributes: ["city_name"],
+          },
+          {
+            model: City,
+            as: "destination_name",
+            attributes: ["city_name"],
+          },
+        ],
+      },
+    ],
+  });
+
+  // console.log(bookingRecords1);
+
+  let allBookingRecords = [];
+
+  flightBookingRecords = JSON.parse(JSON.stringify(flightBookingRecords));
+
+  flightBookingRecords.forEach((element) => {
+    // console.log(element);
+    allBookingRecords.push(element);
+  });
+
+  busBookingRecords.forEach((element) => {
+    allBookingRecords.push(element);
+  });
+
+  trainBookingRecords.forEach((element) => {
+    allBookingRecords.push(element);
+  });
+
+  // allBookingRecords.push(flightBookingRecords);
+  // allBookingRecords.push(busBookingRecords);
+  // allBookingRecords.push(trainBookingRecords);
+
+  // let booking_records_data = bookingRecords;
+
+  // let flightData = [];
+  // let trainData = [];
+  // let busData = [];
+
+  // booking_records_data.forEach((element) => {
+  //   if (element?.transport_type == "flight") {
+  //     flightData.push(element);
+  //   } else if (element?.transport_type == "train") {
+  //     trainData.push(element);
+  //   } else if (element?.transport_type == "bus") {
+  //     busData.push(element);
+  //   }
+  // });
+
+  //   console.log("flight booking records : " + JSON.stringify(flightData));
+  //   console.log("train booking records : " + trainData);
+  //   console.log("bus booking records : " + busData);
+
+  return allBookingRecords;
+};
+
+module.exports = {
+  findBookingRecordsByUserId,
+};
