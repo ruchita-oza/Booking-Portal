93f4a8fefc01d0f2f595ddbfcdc7253ee1c5a80b
RikinChauhan <rikin@atliq.com>
2022-06-13 17:57:46 +0530

    created and tested admin side page for adding train, bus and flight details

---
+++ client/src/Utilities/UsePost.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const UsePost = async (url, data, method = "post") => {
+  try {
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    const response = await axios.post(url, data, { headers });
+
+    return response;
+  } catch (error) {
+    // console.log(error.toString());
+    return error;
+  }
+};
+
+export default UsePost;
